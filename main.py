96eff285930ce961ad4311bf98cb7e3c109a9f83
Daniel Tremura <tremuradaniel@gmail.com>
2022-04-10 11:25:20 +0300

    scrollView

--- main.py
+++ main.py
@@ -7,13 +7,13 @@ from kivy.uix.gridlayout import GridLayout
 from kivy.uix.stacklayout import StackLayout
 from kivy.metrics import dp
 
-NO_OF_BUTTONS = 10
+NO_OF_BUTTONS = 100
 class StackLayoutExample(StackLayout):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.orientation = "lr-bt"
         for i in range(0, NO_OF_BUTTONS):
-            sizes = dp(100) + i*10
+            sizes = dp(100) # + i*10
             b = Button(text=str(i + 1), size_hint=(None, None), size=(sizes,sizes))
             self.add_widget(b)
             
