589957e2971767a2e5eecf4a49ea06956b4486f7
Daniel Tremura <tremuradaniel@gmail.com>
2022-04-16 19:19:48 +0300

    main menu

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ from kivy.app import App
 from kivy.properties import ObjectProperty
 from navigation_screen_manager import NavigationScreenManager
 
-class MyScreenManager(NavigationScreenManager):
+class ScreenManager(NavigationScreenManager):
     pass
 
 class TheLabApp(App):
     manager = ObjectProperty(None)
     def build(self):
-        self.manager = MyScreenManager()
+        self.manager = ScreenManager()
         return self.manager
 
 TheLabApp().run()
